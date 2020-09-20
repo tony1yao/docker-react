f757cc5fcf62d684f5435aafadd1a20e18decdf8
Tony Yao <yaoziliang@163.com>
2020-09-20 18:08:18 +1000

    commit random changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there.
+          Some random changes.
         </p>
         <a
           className="App-link"
